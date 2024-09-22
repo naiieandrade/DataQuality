b7d6528ef4eae320a2b241ffb7b6627f75d186cb
Naiara Andrade <andradenaiara@hotmail.com>
2024-09-22 18:48:45 -0300

    new: create initial modulo to profiling data

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ProjetoTecProg
 Projeto de conclusão desenvolvido para a disciplina de Técnicas de Programação 1 do curso de Engenharia de Dados da Ada Tech
+
+
+## To do
+
+Crie seu dataquality utilizando de POO, crie um módulo que importado para um jupyter notebook crie um relatório como no pandas-profiling/YData-profiling/SweetViz. Neste módulo, tera um método geral para análise de qualquer dataset:
+
+- contagem de nulos
+- contagem de valores únicos
+- value_counts em colunas categóricas
+- describe nas colunas métricas
+- gráficos de distribuição de colunas categóricas
+- gráficos de distribuição de colunas numéricas
---
+++ modulo.py
@@ -0,0 +1,43 @@
+import pandas as pd 
+
+class DataQuality:
+    def __init__(self, path:str):
+        self.path = path
+        self.df = self.read_data()
+
+    def read_data(self):
+        extension = self.path.split('.')[-1]
+        if extension == 'csv':
+            data = pd.read_csv(self.path)
+            return data
+        elif extension == 'json':
+            print('json')
+        else:
+            print(f"Formato /'{extension}/' não reconhecido.")
+
+    def show_columns(self):
+        print(f"Quantidade de colunas: {len(self.df.columns)}\n")
+        n = 1
+
+        print(f"Colunas do dataframe {self.path}\n")
+        for _ in list(self.df.columns):
+            print (f"{n}: {_}")
+            n += 1
+        
+        print('\n')
+        #return self.df.columns
+        
+    def count_nulls(self):
+        print("Quantidade de valores nulos por coluna:")
+        print(self.df.isna().sum())
+
+    def profile(self):
+        print("Iniciando análise dos dados e gerando relatório...")
+        self.show_columns()
+        self.count_nulls()
+
+    def __repr__(self):
+        """
+        Retorna o relatório completo quando o objeto é chamado diretamente no Jupyter Notebook ou terminal.
+        """
+        return self.profile() or ''
